1c8b31cf52532897811933bb42f0aaffe82104d8
carlos-g <carlosg@megacable.com.mx>
2025-03-29 01:03:07 -0600

    Add files via upload

---
+++ Modulo_1_Javascript_Testing/01_sesion/script.js
@@ -0,0 +1,10 @@
+
+var nombre = 'Carlos';
+var apellido = 'Gutierrez';
+var nombreCompleto = `${nombre} ${apellido}`;
+var edad = 10
+
+console.log(`Bienvenido a mi aplicacion ${nombreCompleto}`);
+console.log('Hola, soy ' + nombreCompleto + ' y tengo ' + edad);
+
+
